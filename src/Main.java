7f0f9c87e3c47eeb17e393a24ecb871df7978dd3
Taissirbw <50212633+Taissirbw@users.noreply.github.com>
2021-02-07 19:51:49 +0100

    V0.1 La piste est représentée par une simple ligne brisée fixe et limitée par un horizon, le véhicule est une simple croix qui se déplace d’une valeur fixe lorsqu’on appuie sur les touches

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,16 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String [] args) throws IOException {
-        //Création de la fenetre
-        JFrame fenetre = new JFrame("Une Moto !");
-
-        fenetre.pack();
-        fenetre.setVisible(true);
-        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Etat etat = new Etat(); //Creation du modele
         Affichage affichage = new Affichage(etat); //Création de la vue
-        fenetre.addKeyListener(affichage.control); //gestion des fleches du clavier
+        Control control = new Control(etat, affichage); //Création du contrôle
+
+        //Création de la fenetre
+        JFrame fenetre = new JFrame("Une Moto !");
+        fenetre.addKeyListener(control); //gestion des fleches du clavier
         fenetre.add(affichage); //La vue est ajoutée à la fenetre
         fenetre.pack();
         fenetre.setVisible(true);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
