305e8960ccad7e024e2d728390225ae212531a65
Fanoa <loanrazafimbelo@gmail.com>
2021-02-14 20:57:56 +0100

    Route infinie

---
+++ src/Avancer.java
@@ -0,0 +1,26 @@
+/** Classe Avancer: gestion de l'avancée de l'ovale sur le parcours par le biais d'un thread
+ * */
+public class Avancer extends Thread {
+    Etat etat; //etat du modele
+    Affichage affichage; //affichage dans la fenetre
+    public boolean arret = false; //Condition de lancement du thread
+
+    public Avancer(Etat etat, Affichage affichage){
+        this.etat = etat;
+        this.affichage = affichage;
+        this.arret = false;
+    }
+
+    @Override
+    public void run(){
+        while (!this.arret){ //boucle infinie
+            this.etat.route.updateRoute();
+            affichage.repaint(); //actualisation de l'affichage
+
+            try { Thread.sleep(200); } //Pause
+            catch (Exception e) {
+                Thread.currentThread().interrupt(); //Interruption du thread
+            }
+        }
+    }
+}
