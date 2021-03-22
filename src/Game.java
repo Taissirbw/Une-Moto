67d4d0d07d65bc7cbe4357d1f160818c9e32144a
Fanoa <loanrazafimbelo@gmail.com>
2021-03-22 17:46:03 +0100

    Test écran titre

---
+++ src/Game.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+/** Classe principale */
+public class Game {
+
+    public static void main(String [] args) throws IOException {
+        Etat etat = new Etat(); //Creation du modele
+        Affichage affichage = new Affichage(etat); //Création de la vue
+        Control control = new Control(etat, affichage); //Création du contrôle
+
+        //Création de la fenetre
+        JFrame fenetre = new JFrame("Une Moto !");
+        fenetre.addKeyListener(control); //gestion des fleches du clavier
+        fenetre.add(affichage); //La vue est ajoutée à la fenetre
+        fenetre.pack();
+        fenetre.setVisible(true);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
-/** Classe principale */
-public class Main {
+public class Main extends JPanel implements ActionListener {
+    private JFrame menu;
+    private String title = "Main menu";
+    private  JButton start;
+    public Main(){
+        menu = new JFrame(title);
+        start = new JButton("start");
+        start.setPreferredSize(new Dimension(100,70));
+        start.addActionListener(this);
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(start);
+        menu.getContentPane().add(this);
+        menu.setPreferredSize(new Dimension(300,300));
+        menu.pack();
+        menu.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == start){
+            this.menu.dispose();
+            String[] args1 = {"10"};
+            try {
+                new Game().main(args1);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 
+    }
     public static void main(String [] args) throws IOException {
-        Etat etat = new Etat(); //Creation du modele
-        Affichage affichage = new Affichage(etat); //Création de la vue
-        Control control = new Control(etat, affichage); //Création du contrôle
-
-        //Création de la fenetre
-        JFrame fenetre = new JFrame("Une Moto !");
-        fenetre.addKeyListener(control); //gestion des fleches du clavier
-        fenetre.add(affichage); //La vue est ajoutée à la fenetre
-        fenetre.pack();
-        fenetre.setVisible(true);
-        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        new Main();
     }
 }
