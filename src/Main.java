151ac20d36624c253d4dad52171c6609f53aa62e
Taïssir <50212633+Taissirbw@users.noreply.github.com>
2021-04-07 17:43:45 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,7 @@ public class Main extends JPanel implements ActionListener {
         menu.setLocation(300,33);
 
         //Icone de la fenetre
-        ImageIcon img = new ImageIcon("Assets/moto0.png");
+        ImageIcon img = new ImageIcon("Assets/moto2.png");
         menu.setIconImage(img.getImage());
 
         //Image d'arriere-plan
